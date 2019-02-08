9c35710160179539a588dd34a410ecb493e26bcc
Michael Fladischer <michael@fladi.at>
2019-02-08 12:19:03 +0100

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*names, **kwargs):
 
 setup(
     name='outpost.multicorn',
-    version='0.1.1',
+    version='0.1.2',
     license='BSD',
     description='Outpost Multicorn Extensions',
     long_description='%s\n%s' % (
