fb121a1eda1cf10d44c86c3091d68aac0bb354b8
Michael Fladischer <michael@fladi.at>
2019-05-23 15:33:43 +0200

    Reformat with black.

--- src/outpost/multicorn/fdw.py
+++ src/outpost/multicorn/fdw.py
@@ -11,22 +11,20 @@ logger = logging.getLogger(__name__)
 
 class OutpostFdw(SqlAlchemyFdw):
 
-    engine = {
-        'pool_pre_ping': True,
-    }
+    engine = {"pool_pre_ping": True}
 
     @staticmethod
     def create_engine(url: str):
-        logger.info(f'Calling monkey patched create_engine for {url}')
+        logger.info(f"Calling monkey patched create_engine for {url}")
         return create_engine(url, **OutpostFdw.engine)
 
     def __init__(self, *args, **kwargs):
-        if 'NLS_LANG' not in os.environ:
-            logger.info('NLS_LANG not found in environment, updating ...')
-            os.environ['NLS_LANG'] = '.UTF8'
-        logger.info('Registering custom sqlalchemy dialects ...')
+        if "NLS_LANG" not in os.environ:
+            logger.info("NLS_LANG not found in environment, updating ...")
+            os.environ["NLS_LANG"] = ".UTF8"
+        logger.info("Registering custom sqlalchemy dialects ...")
         registry.register(
-            'oracle.pyodbc', 'outpost.multicorn.pyodbc', 'OracleDialect_pyodbc'
+            "oracle.pyodbc", "outpost.multicorn.pyodbc", "OracleDialect_pyodbc"
         )
         super(OutpostFdw, self).__init__(*args, **kwargs)
 
--- src/outpost/multicorn/pyodbc.py
+++ src/outpost/multicorn/pyodbc.py
@@ -1,8 +1,5 @@
 import re
-from typing import (
-    Tuple,
-    Optional,
-)
+from typing import Tuple, Optional
 
 from sqlalchemy.connectors.pyodbc import PyODBCConnector
 from sqlalchemy.dialects.oracle.base import OracleDialect
@@ -10,16 +7,15 @@ from sqlalchemy.exc import DBAPIError
 
 
 class OracleDialect_pyodbc(PyODBCConnector, OracleDialect):
-
     def _get_server_version_info(self, conn) -> Optional[Tuple[int, ...]]:
         try:
-            raw = conn.scalar('SELECT * FROM v$version;')
+            raw = conn.scalar("SELECT * FROM v$version;")
         except DBAPIError:
             return super()._get_server_version_info(conn)
         else:
-            r = re.compile(r' (?P<version>[\d\.]+) ')
+            r = re.compile(r" (?P<version>[\d\.]+) ")
             matches = r.search(raw)
             if matches:
-                version = matches.groupdict().get('version', '0')
-                return tuple(map(int, version.split('.')))
+                version = matches.groupdict().get("version", "0")
+                return tuple(map(int, version.split(".")))
             return None
